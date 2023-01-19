8224de9fe35574025c342fe8b4ec56c82d849e7c
Bruno <Bruno@DESKTOP-JSLR0PE>
2023-01-19 13:30:13 -0300

    Add project files.

---
+++ GeekShooping.ProductAPI/Migrations/20230119143448_ajusteNome.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeekShooping.ProductAPI.Migrations
+{
+    public partial class ajusteNome : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "cateogry_name",
+                table: "product",
+                newName: "category_name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "category_name",
+                table: "product",
+                newName: "cateogry_name");
+        }
+    }
+}
---
+++ GeekShooping.ProductAPI/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using GeekShooping.ProductAPI.Models.Base;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShooping.ProductAPI.Models
+{
+    [Table("product")]
+    public class Product : BaseEntity
+    {
+        [Column("name")]
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; } = string.Empty;
+
+        [Column("price")]
+        [Required]
+        [Range(1, 10000)]
+        public decimal Price { get; set; }
+
+        [Column("description")]
+        [StringLength(500)]
+        public string? Description { get; set; } = string.Empty;
+
+        [Column("category_name")]
+        [StringLength(60)]
+        public string? CategoryName { get; set; }
+
+        [Column("image_url")]
+        [StringLength(300)]
+        public string? ImageURL { get; set; }
+    }
+}
