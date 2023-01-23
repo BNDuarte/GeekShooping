a668dbc00f9ef45d1f96fcfbb70a896d05133904
Bruno Duarte <brunofduarte@outlook.com>
2023-01-23 11:57:56 -0300

    aula 133.1204

--- GeekShooping.Web/Models/ProductModel.cs
+++ GeekShooping.Web/Models/ProductModel.cs
@@ -8,5 +8,29 @@
         public string? Description { get; set; } = string.Empty;
         public string? CategoryName { get; set; }
         public string? ImageURL { get; set; }
+
+        public string SubstringName()
+        {
+            if (Name.Length < 24)
+            {
+                return Name;
+            }
+            else
+            {
+                return $"{Name.Substring(0, 21)}...";
+            }
+        }
+
+        public string SubstringDescription()
+        {
+            if (Description.Length < 355)
+            {
+                return Description;
+            }
+            else
+            {
+                return $"{Description.Substring(0, 352)}...";
+            }
+        }
     }
 }
