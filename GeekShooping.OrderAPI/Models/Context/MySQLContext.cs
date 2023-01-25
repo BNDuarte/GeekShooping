df6d2f900f6bc808f355d7ec03b0d998018a0d58
Bruno Duarte <brunofduarte@outlook.com>
2023-01-25 15:37:13 -0300

    aula 202.2105

---
+++ GeekShooping.OrderAPI/Models/Context/MySQLContext.cs
@@ -0,0 +1,14 @@
+﻿using GeekShooping.OrderAPI.Models.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShooping.OrderAPI.Models.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
+
+
+        public DbSet<OrderDetail> Details { get; set; }
+        public DbSet<OrderHeader> Headers { get; set; }
+    }
+}
